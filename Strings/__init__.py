624c387af41622cd4e3145a54b9b90fd1203f4b3
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-04 22:45:47 +0530

    Update __init__.py

--- Strings/__init__.py
+++ Strings/__init__.py
@@ -8,21 +8,21 @@ languages_present = {}
 
 
 def get_string(lang: str):
-    return languages[language]
+    return languages[languages]
 
 
-for filename in os.listdir(r"./strings/language/"):
+for filename in os.listdir(r"./strings/languages/"):
     if "english" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/language/english.yml", encoding="utf8")
+        languages["english"] = yaml.safe_load(
+            open(r"./strings/languages/english.yml", encoding="utf8")
         )
         languages_present["english"] = languages["english"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "en":
+        if language_name == "english":
             continue
         languages[language_name] = yaml.safe_load(
-            open(r"./strings/language/" + filename, encoding="utf8")
+            open(r"./strings/languages/" + filename, encoding="utf8")
         )
         for item in languages["english"]:
             if item not in languages[language_name]:
