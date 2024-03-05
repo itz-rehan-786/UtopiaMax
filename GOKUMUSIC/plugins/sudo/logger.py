2061788a5791ef1088baf09ea462d882c5f237d0
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-05 19:16:07 +0530

    Update logger.py

--- GOKUMUSIC/plugins/sudo/logger.py
+++ GOKUMUSIC/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import add_off, add_on
-from DAXXMUSIC.utils.decorators.language import language
+from GOKUMUSIC import app
+from GOKUMUSIC.music import SUDOERS
+from GOKUMUSIC.utils.database import add_off, add_on
+from GOKUMUSIC.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
