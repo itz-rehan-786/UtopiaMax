147ccdc6e7ecf28fb8c0a3ed65c0760e4308e8ae
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-05 19:14:58 +0530

    Update maintenance.py

--- GOKUMUSIC/plugins/sudo/maintenance.py
+++ GOKUMUSIC/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import (
+from GOKUMUSIC import app
+from GOKUMUSIC.music import SUDOERS
+from GOKUMUSIC.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
