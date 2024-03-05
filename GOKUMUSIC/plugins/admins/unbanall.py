33ecfffcb9bf0af7570ba83e278dccd773f9d4cc
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-05 13:27:38 +0530

    Update unbanall.py

--- GOKUMUSIC/plugins/admins/unbanall.py
+++ GOKUMUSIC/plugins/admins/unbanall.py
@@ -2,9 +2,9 @@ from DAXXMUSIC import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from DAXXMUSIC.utils.daxx_ban import admin_filter
+from GOKUMUSIC.utils.daxx_ban import admin_filter
 
-BOT_ID = "6974730627"
+BOT_ID = "" #Yha bot id lgega
 
 @app.on_message(filters.command("unbanall") & admin_filter)
 async def unban_all(_, msg):
