fea30d726c24b51cc58631855ccde63f21440f3e
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-05 18:15:00 +0530

    Update gnmsg.py

--- GOKUMUSIC/plugins/modules/gnmsg.py
+++ GOKUMUSIC/plugins/modules/gnmsg.py
@@ -1,7 +1,7 @@
 import re
 from pyrogram import filters
 import random
-from DAXXMUSIC import app
+from GOKUMUSIC import app
 
 
 @app.on_message(filters.command(["gn","n","oodnight","ood Night","ood night"], prefixes=["/","g","G"]))
