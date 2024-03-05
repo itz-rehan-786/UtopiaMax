d5def10d7e3730beda4a0e43e0d6dd9947ad0e01
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-05 13:48:45 +0530

    Update pause.py

--- GOKUMUSIC/plugins/admins/pause.py
+++ GOKUMUSIC/plugins/admins/pause.py
@@ -3,9 +3,9 @@ from pyrogram.types import Message
 
 from DAXXMUSIC import app
 from DAXXMUSIC.core.call import DAXX
-from DAXXMUSIC.utils.database import is_music_playing, music_off
-from DAXXMUSIC.utils.decorators import AdminRightsCheck
-from DAXXMUSIC.utils.inline import close_markup
+from GOKUMUSIC.utils.database import is_music_playing, music_off
+from GOKUMUSIC.utils.decorators import AdminRightsCheck
+from GOKUMUSIC.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await DAXX.pause_stream(chat_id)
+    await GOKU.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
