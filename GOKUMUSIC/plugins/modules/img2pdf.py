a692eae3a997756a602c9febff91649dc19f1219
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-05 18:11:28 +0530

    Update img2pdf.py

--- GOKUMUSIC/plugins/modules/img2pdf.py
+++ GOKUMUSIC/plugins/modules/img2pdf.py
@@ -7,10 +7,10 @@ from PIL import Image
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.errors import capture_err
+from GOKUMUSIC import app
+from GOKUMUSIC.utils.errors import capture_err
 
-from DAXXMUSIC.core.sections import section
+from GOKUMUSIC.core.sections import section
 
 
 async def convert(
