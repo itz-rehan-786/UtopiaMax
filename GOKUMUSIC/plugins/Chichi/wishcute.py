a16e739c01f9db7701240f5ab13f76de15fbc271
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-05 10:49:10 +0530

    Update wishcute.py

--- GOKUMUSIC/plugins/Chichi/wishcute.py
+++ GOKUMUSIC/plugins/Chichi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from GOKUMUSIC import app 
 
-SUPPORT_CHAT = "ALLTYPECC"
+SUPPORT_CHAT = "goku_groupz"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
