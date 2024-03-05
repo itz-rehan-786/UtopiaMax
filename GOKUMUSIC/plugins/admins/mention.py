835eb3c78aa6739372f060ce84964c8ca08444ff
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-05 13:49:40 +0530

    Update mention.py

--- GOKUMUSIC/plugins/admins/mention.py
+++ GOKUMUSIC/plugins/admins/mention.py
@@ -1,8 +1,8 @@
 import asyncio
 from pyrogram.enums import ChatType, ChatMemberStatus
-from DAXXMUSIC import app
+from GOKUMUSIC import app
 from pyrogram import filters
-from DAXXMUSIC.utils.daxx_ban import admin_filter
+from GOKUMUSIC.utils.daxx_ban import admin_filter
 
 
 
