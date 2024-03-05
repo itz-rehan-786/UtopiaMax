dd4f76a89652c52dc9ade889c25cacf7eb45f1a6
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-05 10:37:12 +0530

    Update mene.py

--- GOKUMUSIC/plugins/Chichi/mene.py
+++ GOKUMUSIC/plugins/Chichi/mene.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import requests
-from DAXXMUSIC import app 
+from GOKUMUSIC import app 
 
 # Define a command handler for the /meme command
 @app.on_message(filters.command("meme"))
