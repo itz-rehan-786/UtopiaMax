815288c09be685cc030e29cf891013918f5503a4
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-05 19:06:54 +0530

    Update channel.py

--- GOKUMUSIC/plugins/play/channel.py
+++ GOKUMUSIC/plugins/play/channel.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import set_cmode
-from DAXXMUSIC.utils.decorators.admins import AdminActual
+from GOKUMUSIC import app
+from GOKUMUSIC.utils.database import set_cmode
+from GOKUMUSIC.utils.decorators.admins import AdminActual
 from config import BANNED_USERS
 
 
