e120becb64e711c268a15ff0b6264588a356a8b4
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-05 14:00:38 +0530

    Update auth.py

--- GOKUMUSIC/plugins/admins/auth.py
+++ GOKUMUSIC/plugins/admins/auth.py
@@ -1,16 +1,16 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils import extract_user, int_to_alpha
-from DAXXMUSIC.utils.database import (
+from GOKUMUSIC import app
+from GOKUMUSIC.utils import extract_user, int_to_alpha
+from GOKUMUSIC.utils.database import (
     delete_authuser,
     get_authuser,
     get_authuser_names,
     save_authuser,
 )
-from DAXXMUSIC.utils.decorators import AdminActual, language
-from DAXXMUSIC.utils.inline import close_markup
+from GOKUMUSIC.utils.decorators import AdminActual, language
+from GOKUMUSIC.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
