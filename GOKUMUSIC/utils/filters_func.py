b79afd46e0f932770e3ad7da6a3ea8fca75dd337
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-06 10:21:50 +0530

    Update filters_func.py

--- GOKUMUSIC/utils/filters_func.py
+++ GOKUMUSIC/utils/filters_func.py
@@ -1,8 +1,8 @@
 from enum import Enum, auto
-from DAXXMUSIC import app
+from GOKUMUSIC import app
 from pyrogram.types import InlineKeyboardMarkup, Message
-from DAXXMUSIC.utils.msg_types import button_markdown_parser
-from DAXXMUSIC.utils.notes_func import NoteFillings
+from GOKUMUSIC.utils.msg_types import button_markdown_parser
+from GOKUMUSIC.utils.notes_func import NoteFillings
 from emojis import decode
 from pyrogram.types import Message
 
