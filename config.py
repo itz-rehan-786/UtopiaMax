6d8ff334639411f78e600e12ee29fc6e92584ec8
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-07 17:01:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/VIPBOLTE/GOKUMUSIC.git",
+    "https://github.com/VIPBOLTE/GOKUMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(" 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"
