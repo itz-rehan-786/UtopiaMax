de7de1c5563eb19bcbebd692af8438a877a61573
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-10 00:06:33 +0530

    Update play.py

--- GOKUMUSIC/plugins/play/play.py
+++ GOKUMUSIC/plugins/play/play.py
@@ -504,7 +504,7 @@ async def play_music(client, CallbackQuery, _):
 
 
 @app.on_callback_query(filters.regex("GOKUmousAdmin") & ~BANNED_USERS)
-async def DAXXmous_check(client, CallbackQuery):
+async def GOKUmous_check(client, CallbackQuery):
     try:
         await CallbackQuery.answer(
             "» ʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ :\n\nᴏᴘᴇɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ sᴇᴛᴛɪɴɢs.\n-> ᴀᴅᴍɪɴɪsᴛʀᴀᴛᴏʀs\n-> ᴄʟɪᴄᴋ ᴏɴ ʏᴏᴜʀ ɴᴀᴍᴇ\n-> ᴜɴᴄʜᴇᴄᴋ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ ᴘᴇʀᴍɪssɪᴏɴs.",
