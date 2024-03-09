e6da58173cfaac4ac8212e44f603ceac3aafdfb3
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-09 19:25:18 +0530

    Update thumbnails.py

--- GOKUMUSIC/utils/thumbnails.py
+++ GOKUMUSIC/utils/thumbnails.py
@@ -107,9 +107,9 @@ async def get_thumb(videoid):
     enhancer = ImageEnhance.Brightness(background)
     background = enhancer.enhance(0.6)
     draw = ImageDraw.Draw(background)
-    arial = ImageFont.truetype("DAXXMUSIC/assets/assets/font2.ttf", 30)
-    font = ImageFont.truetype("DAXXMUSIC/assets/assets/font.ttf", 30)
-    title_font = ImageFont.truetype("DAXXMUSIC/assets/assets/font3.ttf", 45)
+    arial = ImageFont.truetype("GOKUMUSIC/assets/assets/font2.ttf", 30)
+    font = ImageFont.truetype("GOKUMUSIC/assets/assets/font.ttf", 30)
+    title_font = ImageFont.truetype("GOKUMUSIC/assets/assets/font3.ttf", 45)
 
 
     circle_thumbnail = crop_center_circle(youtube, 400, 20)
