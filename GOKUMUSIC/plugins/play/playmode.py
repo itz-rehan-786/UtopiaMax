4aecf64047651b92e1ed48732f7123c02ed04cfb
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-09 16:31:58 +0530

    Update playmode.py

--- GOKUMUSIC/plugins/play/playmode.py
+++ GOKUMUSIC/plugins/play/playmode.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import get_playmode, get_playtype, is_nonadmin_chat
-from DAXXMUSIC.utils.decorators import language
-from DAXXMUSIC.utils.inline.settings import playmode_users_markup
+from GOKUMUSIC import app
+from GOKUMUSIC.utils.database import get_playmode, get_playtype, is_nonadmin_chat
+from GOKUMUSIC.utils.decorators import language
+from GOKUMUSIC.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
 
