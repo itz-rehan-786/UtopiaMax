150678d24603626b0bac2e48beae0a3a13631ebe
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-09 23:46:42 +0530

    Update blchat.py

--- GOKUMUSIC/plugins/sudo/blchat.py
+++ GOKUMUSIC/plugins/sudo/blchat.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
-from DAXXMUSIC.utils.decorators.language import language
+from GOKUMUSIC import app
+from GOKUMUSIC.music import SUDOERS
+from GOKUMUSIC.utils.database import blacklist_chat, blacklisted_chats, whitelist_chat
+from GOKUMUSIC.utils.decorators.language import language
 from config import BANNED_USERS
 
 
