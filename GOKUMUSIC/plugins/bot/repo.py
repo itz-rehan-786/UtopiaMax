1f9df06e4893ceb0b2a31efdd6f2cdd80e824a3e
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-10 08:53:05 +0530

    Update repo.py

--- GOKUMUSIC/plugins/bot/repo.py
+++ GOKUMUSIC/plugins/bot/repo.py
@@ -1,1 +1,34 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from DAXXMUSIC import app
+from config import BOT_USERNAME
+from DAXXMUSIC.utils.errors import capture_err
+import httpx 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+start_txt = """**
+✪ ωεℓ¢σмє ƒσя мя gσкυ яєρσѕ ✪
+ 
+ ➲ ᴀʟʟ ʀᴇᴘᴏ ᴇᴀsɪʟʏ ᴅᴇᴘʟᴏʏ ᴏɴ ʜᴇʀᴏᴋᴜ ᴡɪᴛʜᴏᴜᴛ ᴀɴʏ ᴇʀʀᴏʀ ✰
+ 
+ ➲ ɴᴏ ʜᴇʀᴏᴋᴜ ʙᴀɴ ɪssᴜᴇ ✰
+ 
+ ➲ ɴᴏ ɪᴅ ʙᴀɴ ɪssᴜᴇ ✰
+ 
+ ➲ᴜɴʟɪᴍɪᴛᴇᴅ ᴅʏɴᴏs ✰
+ 
+ ➲ ʀᴜɴ 24x7 ʟᴀɢ ғʀᴇᴇ ᴡɪᴛʜᴏᴜᴛ sᴛᴏᴘ ✰
+ 
+ ► ɪғ ʏᴏᴜ ғᴀᴄᴇ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ ᴛʜᴇɴ sᴇɴᴅ ss
+**"""
+
+
+
+
+@app.on_message(filters.command("repo"))
+async def start(_, msg):
+    buttons = [
+        [ 
+          InlineKeyboardButton("𝗬𝗘 𝗟𝗘 𝗟𝗢", url=f"https://telegra.ph/file/eae68a6b6422567fe9971.mp4")
+        ]
+    ]   
