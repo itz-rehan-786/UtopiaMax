8607f82d7cdff536b1379ab190271f61de9a2113
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-10 14:34:18 +0530

    Update repo.py

--- GOKUMUSIC/plugins/bot/repo.py
+++ GOKUMUSIC/plugins/bot/repo.py
@@ -17,16 +17,14 @@ async def start(_, msg):
         caption=start_txt,
         reply_markup=reply_markup
     )
- 
-   
-# --------------
 
-
-@app.on_message(filters.command("repo", prefixes="#"))
+    
+#_______________
+app.on_message(filters.command("repo", prefixes="#"))
 @capture_err
 async def repo(_, message):
     async with httpx.AsyncClient() as client:
-        response = await client.get("https://api.github.com/repos/VIPBOLTE")
+        response = await client.get("https://api.github.com/repos/DAXXTEAM/DAXXMUSIC/contributors")
     
     if response.status_code == 200:
         users = response.json()
@@ -36,7 +34,7 @@ async def repo(_, message):
             list_of_users += f"{count}. [{user['login']}]({user['html_url']})\n"
             count += 1
 
-        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://github.com/VIPBOLTE/GOKUMUSIC.git) | [𝖦𝖱𝖮𝖴𝖯](https://t.me/goku_groupz)
+        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://github.com/DAXXTEAM/DAXXMUSIC) | [𝖦𝖱𝖮𝖴𝖯](https://t.me/HEROKUFREECC)
 | 𝖢𝖮𝖭𝖳𝖱𝖨𝖡𝖴𝖳𝖮𝖱𝖲 |
 ----------------
 {list_of_users}"""
