e4d05edf857e3b166ab89d31e8a007cdd6bdee27
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-10 14:53:48 +0530

    Update repo.py

--- GOKUMUSIC/plugins/bot/repo.py
+++ GOKUMUSIC/plugins/bot/repo.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from DAXXMUSIC import app
+from GOKUMUSIC import app
 from config import BOT_USERNAME
-from DAXXMUSIC.utils.errors import capture_err
+from GOKUMUSIC.utils.errors import capture_err
 import httpx 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
