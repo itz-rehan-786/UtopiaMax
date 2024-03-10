ef88584650d286a1dc6c2b00df6dadec0dca37a0
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-10 09:05:23 +0530

    Update mustjoin.py

--- GOKUMUSIC/plugins/bot/mustjoin.py
+++ GOKUMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from GOKUMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "goopu_group"
+MUST_JOIN = "channelz_k"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
