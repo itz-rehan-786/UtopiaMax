b9ff5907d955aa21b871396f2197eae491d99cae
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-10 12:53:20 +0530

    Update repo.py

--- GOKUMUSIC/plugins/bot/repo.py
+++ GOKUMUSIC/plugins/bot/repo.py
@@ -31,4 +31,39 @@ async def start(_, msg):
         [ 
           InlineKeyboardButton("𝗬𝗘 𝗟𝗘 𝗟𝗢", url=f"https://telegra.ph/file/eae68a6b6422567fe9971.mp4")
         ]
-    ]   
+     
+    ]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await msg.reply_photo(
+        photo="https://telegra.ph/file/a0c824b3ad40e8bd86db7.jpg",
+        caption=start_txt,
+        reply_markup=reply_markup
+    )
+ 
+   
+# --------------
+
+
+@app.on_message(filters.command("repo", prefixes="#"))
+@capture_err
+async def repo(_, message):
+    async with httpx.AsyncClient() as client:
+        response = await client.get("https://api.github.com/repos/VIPBOLTE")
+    
+    if response.status_code == 200:
+        users = response.json()
+        list_of_users = ""
+        count = 1
+        for user in users:
+            list_of_users += f"{count}. [{user['login']}]({user['html_url']})\n"
+            count += 1
+
+        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://github.com/VIPBOLTE/GOKUMUSIC.git) | [𝖦𝖱𝖮𝖴𝖯](https://t.me/goku_groupz)
+| 𝖢𝖮𝖭𝖳𝖱𝖨𝖡𝖴𝖳𝖮𝖱𝖲 |
+----------------
+{list_of_users}"""
+        await app.send_message(message.chat.id, text=text, disable_web_page_preview=True)
+    else:
+        await app.send_message(message.chat.id, text="Failed to fetch contributors.")
