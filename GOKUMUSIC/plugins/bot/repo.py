183277e275dd331956dd733aa6ef960908ff3abc
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-10 14:48:38 +0530

    Update repo.py

--- GOKUMUSIC/plugins/bot/repo.py
+++ GOKUMUSIC/plugins/bot/repo.py
@@ -1,37 +1,58 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from GOKUMUSIC import app
+from DAXXMUSIC import app
 from config import BOT_USERNAME
-from GOKUMUSIC.utils.errors import capture_err
+from DAXXMUSIC.utils.errors import capture_err
 import httpx 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 start_txt = """**
-✪ ωεℓ¢σмє ƒσя мя gσкυ яєρσѕ ✪
+✪ ωεℓ¢σмє ƒσя 𝚍𝚊𝚡𝚡 яєρσѕ ✪
+ 
+ ➲ ᴀʟʟ ʀᴇᴘᴏ ᴇᴀsɪʟʏ ᴅᴇᴘʟᴏʏ ᴏɴ ʜᴇʀᴏᴋᴜ ᴡɪᴛʜᴏᴜᴛ ᴀɴʏ ᴇʀʀᴏʀ ✰
+ 
+ ➲ ɴᴏ ʜᴇʀᴏᴋᴜ ʙᴀɴ ɪssᴜᴇ ✰
+ 
+ ➲ ɴᴏ ɪᴅ ʙᴀɴ ɪssᴜᴇ ✰
+ 
+ ➲ᴜɴʟɪᴍɪᴛᴇᴅ ᴅʏɴᴏs ✰
+ 
+ ➲ ʀᴜɴ 24x7 ʟᴀɢ ғʀᴇᴇ ᴡɪᴛʜᴏᴜᴛ sᴛᴏᴘ ✰
+ 
+ ► ɪғ ʏᴏᴜ ғᴀᴄᴇ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ ᴛʜᴇɴ sᴇɴᴅ ss
+**"""
+
+
+
+
 @app.on_message(filters.command("repo"))
 async def start(_, msg):
-buttons = [
+    buttons = [
         [ 
           InlineKeyboardButton("𝗔𝗗𝗗 𝗠𝗘", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("𝗛𝗘𝗟𝗣", url="https://t.me/goku_groupz"),
-          InlineKeyboardButton("𝗢𝗪𝗡𝗘𝗥", url="https://t.me/Zindagi_hai_tere_nal"),
-          ],
-
+          InlineKeyboardButton("𝗛𝗘𝗟𝗣", url="https://t.me/channelz_k"),
+          InlineKeyboardButton("𝗢𝗪𝗡𝗘𝗥", url="https://t.me/iam_daxx"),
+        ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
     await msg.reply_photo(
-        photo="https://telegra.ph/file/eae68a6b6422567fe9971.mp4",
+        photo="https://telegra.ph/file/faa1f3ad7116e33d9f402.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
+ 
+   
+# --------------
 
-    
-#_______________
-app.on_message(filters.command("repo", prefixes="#"))
+
+@app.on_message(filters.command("repo", prefixes="#"))
 @capture_err
 async def repo(_, message):
     async with httpx.AsyncClient() as client:
-        response = await client.get("https://api.github.com/repos/DAXXTEAM/DAXXMUSIC/contributors")
+        response = await client.get("https://api.github.com/repos/GOKUMUSIC/VIPBOLTE/contributors")
     
     if response.status_code == 200:
         users = response.json()
