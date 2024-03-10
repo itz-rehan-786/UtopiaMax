8ab4b7a845400d039599c5a8abcff8d0ed83b33d
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-10 11:53:03 +0530

    Update userbot.py

--- GOKUMUSIC/core/userbot.py
+++ GOKUMUSIC/core/userbot.py
@@ -52,7 +52,7 @@ class Userbot(Client):
             await self.one.start()
             try:
                 await self.one.join_chat("goku_groupz")
-                await self.one.join_chat("goopu_group")
+                await self.one.join_chat("channelz_k")
             except:
                 pass
             assistants.append(1)
@@ -73,7 +73,7 @@ class Userbot(Client):
             await self.two.start()
             try:
                 await self.two.join_chat("goku_groupz")
-                await self.one.join_chat("goopu_group")
+                await self.one.join_chat("channelz_k")
             except:
                 pass
             assistants.append(2)
@@ -94,7 +94,7 @@ class Userbot(Client):
             await self.three.start()
             try:
                 await self.three.join_chat("goku_groupz")
-                await self.one.join_chat("goopu_group")
+                await self.one.join_chat("channelz_k")
             except:
                 pass
             assistants.append(3)
@@ -115,7 +115,7 @@ class Userbot(Client):
             await self.four.start()
             try:
                 await self.four.join_chat("goku_groupz")
-                await self.one.join_chat("goopu_group")
+                await self.one.join_chat("channelz_k")
             except:
                 pass
             assistants.append(4)
@@ -136,7 +136,7 @@ class Userbot(Client):
             await self.five.start()
             try:
                 await self.five.join_chat("goku_groupz")
-                await self.one.join_chat("goopu_group")
+                await self.one.join_chat("channelz_k")
             except:
                 pass
             assistants.append(5)
