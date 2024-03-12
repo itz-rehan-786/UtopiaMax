b59b6f39a72840fcc0fd49d7b52a2c6febc13dea
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-13 02:19:07 +0530

    Update userbot.py

--- GOKUMUSIC/core/userbot.py
+++ GOKUMUSIC/core/userbot.py
@@ -11,35 +11,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="GOKUAss1",
+            name="DAXXAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="GOKUAss2",
+            name="DAXXAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="GOKUAss3",
+            name="DAXXAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="GOKUAss4",
+            name="DAXXAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="GOKUAss5",
+            name="DAXXAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("goku_groupz")
-                await self.one.join_chat("channelz_k")
+                await self.one.join_chat("ALLTYPECC")
+                await self.one.join_chat("DAXXSUPPORT")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("goku_groupz")
-                await self.one.join_chat("channelz_k")
+                await self.two.join_chat("ALLTYPECC")
+                await self.one.join_chat("DAXXSUPPORT")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("goku_groupz")
-                await self.one.join_chat("channelz_k")
+                await self.three.join_chat("ALLTYPECC")
+                await self.one.join_chat("DAXXSUPPORT")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("goku_groupz")
-                await self.one.join_chat("channelz_k")
+                await self.four.join_chat("ALLTYPECC")
+                await self.one.join_chat("DAXXSUPPORT")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("goku_groupz")
-                await self.one.join_chat("channelz_k")
+                await self.five.join_chat("ALLTYPECC")
+                await self.one.join_chat("DAXXSUPPORT")
             except:
                 pass
             assistants.append(5)
