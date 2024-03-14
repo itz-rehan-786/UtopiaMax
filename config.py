82b2609f633d30759f86ac27083aa657c500588b
VIPBOLTE <154590485+VIPBOLTE@users.noreply.github.com>
2024-03-14 10:14:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/VIPBOLTE/GOKUMUSIC",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # ----------------------------------------------------------------
