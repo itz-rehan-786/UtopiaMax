f707a58007f002c69a03f2937b4fdf3c7e2c9214
ishu9805 <abhishek47903@gmail.com>
2024-03-15 14:24:43 +0530

    Update upscale.py

--- GOKUMUSIC/plugins/modules/upscale.py
+++ GOKUMUSIC/plugins/modules/upscale.py
@@ -17,7 +17,7 @@ import numpy as np
 async def upscale_image(app, message):
     try:
         if not message.reply_to_message or not message.reply_to_message.photo:
-            await message.reply_text("Please reply to an image to upscale it.")
+            await message.reply_text("**Please reply to an image to upscale it.**")
             return
 
         image = message.reply_to_message.photo.file_id
@@ -41,7 +41,7 @@ async def upscale_image(app, message):
         await app.send_photo(
             message.chat.id,
             photo=sharpened_file_path,
-            caption="Here is the upscaled and sharpened image!",
+            caption="**Here is the upscaled and sharpened image!**",
         )
 
         # Clean up files
@@ -49,39 +49,5 @@ async def upscale_image(app, message):
         os.remove(sharpened_file_path)
 
     except Exception as e:
-
-# ------------
-
-
-waifu_api_url = 'https://api.waifu.im/search'
-
-# Zindagi_hai_tere_nal
-
-def get_waifu_data(tags):
-    params = {
-        'included_tags': tags,
-        'height': '>=2000'
-    }
-
-    response = requests.get(waifu_api_url, params=params)
-
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return None
-
-@app.on_message(filters.command("waifu"))
-def waifu_command(client, message):
-    try:
-        tags = ['maid']  # You can customize the tags as needed
-        waifu_data = get_waifu_data(tags)
-
-        if waifu_data and 'images' in waifu_data:
-            first_image = waifu_data['images'][0]
-            image_url = first_image['url']
-            message.reply_photo(image_url)
-        else:
-            message.reply_text("No waifu found with the specified tags.")
-
-    except Exception as e:
-        message.reply_text(f"An error occurred: {str(e)}")
+        print(f"Failed to upscale and sharpen the image: {e}")
+        await message.reply_text("**Failed to upscale and sharpen the image. Please try again later.**")
