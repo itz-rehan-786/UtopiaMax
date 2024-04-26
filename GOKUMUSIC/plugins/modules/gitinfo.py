1af703c0f6d4aa1d64f2ea71d8d74632c298a7b4
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2024-04-26 23:37:05 +0530

    Update gitinfo.py

--- GOKUMUSIC/plugins/modules/gitinfo.py
+++ GOKUMUSIC/plugins/modules/gitinfo.py
@@ -1,7 +1,7 @@
 import asyncio, os, time, aiohttp
 import aiohttp
 from pyrogram import filters
-from GOKUhub import GOKUhub as papaGOKU
+from daxxhub import daxxhub as papaGOKU
 from GOKUMUSIC import app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
