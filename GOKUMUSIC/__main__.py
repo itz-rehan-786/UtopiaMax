f0eda819f7737ffb6eee6a22f2e0447a1f958cb4
Zeta-05 <vjimmy276@gmail.com>
2024-11-23 17:41:18 +0530

    Update __main__.py

--- GOKUMUSIC/__main__.py
+++ GOKUMUSIC/__main__.py
@@ -43,19 +43,19 @@ async def init():
         await GOKU.stream_call("https://telegra.ph/file/289cb2d7166b7e2d6c1a8.mp4")
     except NoActiveGroupCall:
         LOGGER("GOKUMUSIC").error(
-            "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗚𝗢𝗞𝗨 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
+            "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗨𝗧𝗢𝗣𝗜𝗔 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
         )
         exit()
     except:
         pass
     await GOKU.decorators()
     LOGGER("GOKUMUSIC").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗥 𝗚𝗢𝗞𝗨☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+        "╔═════ஜ۩۞۩ஜ════╗\n𝗠𝗔𝗗𝗘 𝗕𝗬 𝗧𝗨𝗛𝗜𝗡\n╚═════ஜ۩۞۩ஜ════╝"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("GOKUMUSIC").info("𝗦𝗧𝗢𝗣 𝗚𝗢𝗞𝗨 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
+    LOGGER("GOKUMUSIC").info("𝗦𝗧𝗢𝗣 𝗨𝗧𝗢𝗣𝗜𝗔..")
 
 
 if __name__ == "__main__":
