d8517e57b2532b137fea2877934ac86ae6064237
Zeta-05 <vjimmy276@gmail.com>
2024-11-26 11:44:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ SONG_DOWNLOAD_DURATION_LIMIT = get_env_int("SONG_DOWNLOAD_DURATION_LIMIT", 99999
 
 # Owner and logging
 # Chat id of a group for logging bot's activities
-LOGGER_ID = os.getenv("LOGGER_ID", -1002324830715)
+LOGGER_ID = os.getenv("LOGGER_ID", -1002126989582)
 OWNER_ID = get_env_int("OWNER_ID", 7678359785)
 
 # Heroku configurations
