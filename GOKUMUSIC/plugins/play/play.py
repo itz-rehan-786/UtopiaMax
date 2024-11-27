2f7a35c74ebfc0bd4bab2fe49600066900d7d450
Zeta-05 <vjimmy276@gmail.com>
2024-11-27 16:00:15 +0530

    Update play.py

--- GOKUMUSIC/plugins/play/play.py
+++ GOKUMUSIC/plugins/play/play.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from GOKUMUSIC import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from GOKUMUSIC import Apple, Resso, SoundCloud, Spotify, Telegram, Youtube, app
 from GOKUMUSIC.core.call import GOKU
 from GOKUMUSIC.utils import seconds_to_min, time_to_seconds
 from GOKUMUSIC.utils.channelplay import get_channeplayCB
