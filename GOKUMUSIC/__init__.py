fa2a2985ac07fedc2c4b39229fc92f312aa46bd5
Zeta-05 <vjimmy276@gmail.com>
2024-11-27 17:55:44 +0530

    Create __init__.py

---
+++ GOKUMUSIC/__init__.py
@@ -0,0 +1,26 @@
+from GOKUMUSIC.core.bot import GOKU
+from GOKUMUSIC.core.dir import dirr
+from GOKUMUSIC.core.git import git
+from GOKUMUSIC.core.userbot import Userbot
+from GOKUMUSIC.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Aviax()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
