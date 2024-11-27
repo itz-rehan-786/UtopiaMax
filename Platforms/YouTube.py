3034f0ed12d9d7d1fe8f2bd69f6cded5f84e0d78
Zeta-05 <vjimmy276@gmail.com>
2024-11-27 18:22:42 +0530

    Update YouTube.py

--- Platforms/YouTube.py
+++ Platforms/YouTube.py
@@ -9,8 +9,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AviaxMusic.utils.database import is_on_off
-from AviaxMusic.utils.formatters import time_to_seconds
+from GOKUMUSIC.utils.database import is_on_off
+from GOKUMUSIC.utils.formatters import time_to_seconds
 
 
 
